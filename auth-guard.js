75616cac32dba18a1f171934c2c8ae2820f2dcc0
alethia <nouredine.m22@gmail.com>
2025-10-13 23:49:07 +0100

    Update auth-guard.js

--- auth-guard.js
+++ auth-guard.js
@@ -5,7 +5,7 @@ import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/fi
 // Vérifie si l'utilisateur est connecté
 onAuthStateChanged(auth, (user) => {
   if (!user) {
-    // Si l'utilisateur n'est pas connecté, redirection vers login
-    window.location.href = "../login.html"; // ajustez le chemin selon l'emplacement
+    // Redirection vers login.html
+    window.location.href = "/login.html";
   }
 });
