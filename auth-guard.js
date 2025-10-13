ed9a0a6fcca23ef5b56153e6ed5eee76f8646223
alethia <nouredine.m22@gmail.com>
2025-10-13 23:19:20 +0100

    Update auth-guard.js

--- auth-guard.js
+++ auth-guard.js
@@ -1,10 +1,11 @@
 // auth-guard.js
 import { auth } from "./firebase-config.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
+// Vérifie si l'utilisateur est connecté
 onAuthStateChanged(auth, (user) => {
   if (!user) {
-    alert("⚠️ Vous devez être connecté pour accéder à cette page.");
-    window.location.href = "../../login.html"; 
+    // Si l'utilisateur n'est pas connecté, redirection vers login
+    window.location.href = "../login.html"; // ajustez le chemin selon l'emplacement
   }
 });
