32680ba2aa19490b8f0005dd76f9ffb842d750ed
alethia <nouredine.m22@gmail.com>
2025-10-16 20:32:40 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,15 @@
-// === firebase-config.js ===
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuwjW3DjLYvo2gorazMA99qPam4mQ3D5k",
-  authDomain: "alethiamessage.firebaseapp.com",
+  apiKey: "...",
+  authDomain: "...",
   projectId: "alethiamessage",
-  storageBucket: "alethiamessage.firebasestorage.app",
-  messagingSenderId: "1093724154743",
-  appId: "1:1093724154743:web:e90669c98f47dc64d48f72",
-  measurementId: "G-4YJ1K1Y7FT"
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
+  measurementId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
